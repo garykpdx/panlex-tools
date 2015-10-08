98401a6a2673c90dd21c0f5c977d39dbc7f6329b
Gary Krug <garykpdx145@gmail.com>
2015-10-08 10:06:12 -0700

    added DelimiterFilter - doesn't do anything yet

--- processing/sourcefilter.py
+++ processing/sourcefilter.py
@@ -9,7 +9,12 @@ class SourceProcesor(object):
 
 
 
-class PunctFilter(SourceProcesor):
-    def run(self):
-        pass
+class DelimiterFilter(SourceProcesor):
+    def __init__(self, delim):
+        self.delimiters = delim
+
+
+    def run(self, text):
+        delims = '[%s]' % self.delimiters
+        return re.sub(delims, '‣', text)
         
